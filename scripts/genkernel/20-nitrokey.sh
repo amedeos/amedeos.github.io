d3de77c1ea1a4dfc9611ea2be962d84adc58956f
Amedeo Salvati <amedeo@linux.com>
2020-07-26 18:28:57 +0200

    add 20-nitrokey.sh

---
+++ scripts/genkernel/20-nitrokey.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+source /etc/genkernel.conf
+rm -rf ${INITRAMFS_OVERLAY}
+mkdir -p ${INITRAMFS_OVERLAY}/{usr/lib64,usr/bin,lib64,bin,etc}
+NITROLUKS="https://github.com/artosan/nitroluks"
+INITRD_PATCH="https://raw.githubusercontent.com/amedeos/amedeos.github.io/master/scripts/genkernel/initrd.scripts.patch"
+GENKERNEL_DIR="/usr/share/genkernel/defaults"
+CRYPT_FILE="initrd.scripts"
+GIT_BIN=$(which git)
+GPLUS_BIN=$(which g++)
+CURL_BIN=$(which curl)
+LDD_BIN=$(which ldd)
+#TODO: insert return codes and check them after every commands
+
+NITROBUILD=$(mktemp -t -d nitrobuild.XXXXX)
+${GIT_BIN} clone ${NITROLUKS} ${NITROBUILD}
+mkdir -p ${NITROBUILD}/build
+${GPLUS_BIN} ${NITROBUILD}/src/nitro_luks.c -o ${NITROBUILD}/build/nitro_luks -L${NITROBUILD}/build/ -l:libnitrokey.so.3 -Wall
+cp ${NITROBUILD}/build/nitro_luks ${INITRAMFS_OVERLAY}/bin/
+
+for f in $(ldd ${NITROBUILD}/build/nitro_luks | egrep "=>" |awk '{print $3}'); do
+	echo "Copy shared libraries $f"
+	mkdir -p "${INITRAMFS_OVERLAY}/$(dirname $f)"
+	cp --dereference ${f}* ${INITRAMFS_OVERLAY}/$(dirname $f)/
+done
+
+${CURL_BIN} --output ${NITROBUILD}/initrd.scripts.patch ${INITRD_PATCH}
+cp ${GENKERNEL_DIR}/${CRYPT_FILE} ${NITROBUILD}/${CRYPT_FILE}
+patch ${NITROBUILD}/${CRYPT_FILE} ${NITROBUILD}/initrd.scripts.patch
+cp ${NITROBUILD}/${CRYPT_FILE} ${INITRAMFS_OVERLAY}/etc/${CRYPT_FILE}
+rm -rf ${NITROBUILD}
+# we need to raise this file in the future, otherwise genkernel will overwrite it in initramfs
+TZ=ZZZ0 touch -t "$(TZ=ZZZ-12:00 date +%Y%m%d%H%M.%S)" ${INITRAMFS_OVERLAY}/etc/${CRYPT_FILE}
